c3490fa4f2e5b3290dbce6f06214ff5214bb5ec5
cmill355 <noneofya09@gmail.com>
2025-02-24 16:14:17 -0500

    finished

--- src/main/java/com/codedifferently/lab/partB/Coin.java
+++ src/main/java/com/codedifferently/lab/partB/Coin.java
@@ -1,4 +1,29 @@
 package com.codedifferently.lab.partB;
 
 public class Coin {
+    private String name;
+    private double value;
+
+    public Coin(String name, double value) {
+        this.name= name;
+        this.value= value;
+    }
+
+    public Coin (){
+        this.name = "Penny";
+        this.value = 0.01;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return name + " ($" + value + ")";
+    }
 }
--- src/main/java/com/codedifferently/lab/partB/Main.java
+++ src/main/java/com/codedifferently/lab/partB/Main.java
@@ -1,4 +1,35 @@
 package com.codedifferently.lab.partB;
 
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Coin> coins = new ArrayList<>();
+        coins.add(new Coin());
+        coins.add(new Coin("Quarter", .25));
+        coins.add(new Coin("Dime", .10));
+        coins.add(new Coin("Penny", .01));
+
+        System.out.println("Original Coins");
+        for (Coin coin: coins) {
+            System.out.println(coin);
+        }
+
+        Collections.sort(coins, Comparator.comparingDouble(Coin::getValue));
+
+        System.out.println("\nSorted Coins (by value):");
+        for (Coin coin: coins) {
+            System.out.println(coin);
+        }
+
+        Map<String, Integer> coinCount = new LinkedHashMap<>();
+        for (Coin coin : coins) {
+            coinCount.put(coin.getName(), coinCount.getOrDefault(coin.getName(),0) + 1);
+        }
+
+        System.out.println("\nYou have:");
+        for (Map.Entry<String, Integer> entry : coinCount.entrySet()) {
+            System.out.println(entry.getValue() + " " + entry.getKey() + "coin" + (entry.getValue() > 1 ? "s" : ""));
+        }
+        }
 }
